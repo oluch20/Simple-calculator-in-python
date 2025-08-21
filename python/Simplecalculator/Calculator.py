081d639f0abcd3458b234a599e0c1fab9c74c765
oluch20 <olekmlot321@gmail.com>
2025-08-21 19:59:21 +0200

    some starting point

--- python/Simplecalculator/Calculator.py
+++ python/Simplecalculator/Calculator.py
@@ -1,2 +1,30 @@
-a=2
-print(a)
+def add():
+    global answer
+    answer=(first_numer + second_number)
+
+def subtract():
+    global answer
+    answer=(first_numer - second_number)
+
+
+
+print("Enter number:")
+
+first_numer=float(input()) # first number
+
+print("Enter equation:")
+equation=input() # equation
+print("Enter second number:")
+second_number=float(input()) # second number
+
+
+if equation == "+":
+    add()
+elif equation =="-":
+    subtract()
+else:
+    print("I don't understand equation")
+
+
+print("Your answer is:")
+print(answer)
