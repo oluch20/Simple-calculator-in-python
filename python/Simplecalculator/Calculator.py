49ee823db3e16cb608be27aed9f5cafbf6528d09
oluch20 <olekmlot321@gmail.com>
2025-08-21 22:56:24 +0200

    Refactor calculator functions to improve input handling and remove unused functions

--- python/Simplecalculator/Calculator.py
+++ python/Simplecalculator/Calculator.py
@@ -1,30 +1,47 @@
-def add():
-    global answer
-    answer=(first_numer + second_number)
+acceptable_equations = ["+", "-", "*", "/"]
 
-def subtract():
-    global answer
-    answer=(first_numer - second_number)
+def is_number(number):
+    try:
+        number = int(number)
+    except:
+        try:
+            number = float(number)
+        except:
+            print("It's not a number, try again")
+            print("")
+            number =input()
+            is_number(number)
 
-
+def is_equation():
+    if equation is not acceptable_equations:
+        print("Wrong equation")
+        equation=input()
+        is_equation()
 
 print("Enter number:")
-
-first_numer=float(input()) # first number
+first_numer=input() # first number
+is_number(first_numer)
 
 print("Enter equation:")
 equation=input() # equation
-print("Enter second number:")
-second_number=float(input()) # second number
+is_equation()
 
+print("Enter second number:")
+second_number=input() # second number
+is_number(second_number)
 
 if equation == "+":
-    add()
-elif equation =="-":
-    subtract()
+    answer=(first_numer + second_number)
+elif equation == "-":
+    answer=(first_numer - second_number)
+elif equation == "*":
+    answer=(first_numer * second_number)
+elif equation == "/":
+    answer=(first_numer / second_number)
 else:
     print("I don't understand equation")
 
+function(equation)
 
 print("Your answer is:")
 print(answer)
